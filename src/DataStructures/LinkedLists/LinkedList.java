3febe75488e0ff82054cadc116938918cfdd5064
VenkatRaja-Ch <chvenkatraja.work@gmail.com>
2023-07-17 08:16:42 +0300

    LinkedList created using the LinkedListNode.

---
+++ src/DataStructures/LinkedLists/LinkedList.java
@@ -0,0 +1,71 @@
+package DataStructures.LinkedLists;
+
+import Nodes.LinkedListNode;
+
+public class LinkedList <K extends Comparable<K>, V> {
+
+    private LinkedListNode<K, V> head;
+    private int size;
+
+    public LinkedList(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    // insertion operation
+    public void insertion(K key, V value){
+
+        LinkedListNode<K, V> newNode = new LinkedListNode<>(key, value);
+
+        if(isEmpty()){
+            head = newNode;
+        } else {
+            LinkedListNode<K, V> currentNode = head;
+            LinkedListNode<K, V> previousNode = null;
+
+            while(currentNode!=null && currentNode.getKey().compareTo(key) > 0) {
+                previousNode = currentNode;
+                currentNode = currentNode.getNextNode();
+            }
+
+            if(previousNode == null) {
+                newNode.setNextNode(head);
+                head = newNode;
+            } else {
+                previousNode.setNextNode(newNode);
+                newNode.setNextNode(currentNode);
+            }
+        }
+
+        size ++;
+    }
+
+    // deletion operation
+    public V deletion(){
+
+        if(isEmpty())
+            throw new IllegalStateException("Priority Queue is empty!");
+
+        LinkedListNode<K, V> maxPriorityNode = head;
+        head = head.getNextNode();
+        maxPriorityNode.setNextNode(null);
+        size --;
+
+        return maxPriorityNode.getValue();
+    }
+
+    public V peek(){
+        if(isEmpty())
+            throw new IllegalStateException("Priority Queue is empty!");
+
+        return head.getValue();
+    }
+
+    private boolean isEmpty(){
+        return (this.size == 0);
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+}
